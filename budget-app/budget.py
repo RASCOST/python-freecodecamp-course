576c7e5ee97f9b1b7619ad0578eadf571c5a0c6a
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-03-17 21:47:49 +0100

    created transfer

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,13 +1,14 @@
 class Category:
-    def __init__(self, ledger):
-        self.ledger = ledger
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
 
 
     def deposit(self, amount, comment=''):
         ledger.append({'amount': amount, 'comment': comment})
 
     def withdraw(self,  amount, comment=''):
-        if get_balance() >= amount:
+        if get_balance() > amount:
             deposit(-amount, comment)
             return true
         return false
@@ -19,3 +20,12 @@ class Category:
             total += self.ledger[index]['amount']
 
         return total
+
+    def transfer(self, amount, category):
+        if get_balance() > amount:
+            withdraw(amout, f"Transfert to {category.name}")
+            deposit(amout, f"Transfert from {self.name}")
+            return true
+
+        return false
+
