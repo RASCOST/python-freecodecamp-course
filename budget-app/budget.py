c77afc9088b2ad41ca9bd28e086a26f7e40b07af
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-03-17 21:24:54 +0100

    created withdraw

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,9 +1,13 @@
 class Category:
-    def __init__(ledger):
+    def __init__(self, ledger):
         self.ledger = ledger
 
 
     def deposit(self, amount, comment=''):
         ledger.append({'amount': amount, 'comment': comment})
 
-    
+    def withdraw(self,  amount, comment=''):
+        if get_balance() >= amount:
+            deposit(-amount, comment)
+            return true
+        return false
