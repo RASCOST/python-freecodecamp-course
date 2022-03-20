e683b032769081ca44a88b994a853c3e6d12ede6
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-03-20 22:33:21 +0100

    created method title, list_items and total

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -5,18 +5,18 @@ class Category:
 
 
     def deposit(self, amount, comment=''):
-        self.ledger.append({'amount': amount, 'comment': comment})
+        self.ledger.append({'amount': amount, 'description': comment})
 
     def withdraw(self,  amount, comment=''):
         if self.check_funds(amount):
             self.deposit(-amount, comment)
-            return true
-        return false
+            return True
+        return False
 
     def get_balance(self):
         total = 0
 
-        for index=0 in len(self.ledger):
+        for index in range(len(self.ledger) - 1):
             total += self.ledger[index]['amount']
 
         return total
@@ -25,24 +25,54 @@ class Category:
         if self.check_funds(amount):
             self.withdraw(amout, f"Transfer to {category.name}")
             self.deposit(amout, f"Transfer from {self.name}")
-            return true
+            return True
 
-        return false
+        return False
 
     def check_funds(self, amount):
         if amount > self.get_balance():
-            return false
+            return False
 
-        return true
+        return True
 
-    def __str__(self):
+    def title(self):
         cat_length = len(self.name)
 
         if 30 - cat_length % 2 == 0:
-            title = '*' * (30 - cat_length) / 2
+            title = '*' * int((30 - cat_length) / 2)
             title += self.name
-            title += '*' * (30 - cat_length) / 2
+            title += '*' * int((30 - cat_length) / 2)
         else:
-            title = '*' * (30 - cat_length) / 2
+            title = '*' * int((30 - cat_length) / 2)
             title += self.name
-            title += '*' * (30 - ((30 - cat_length) / 2) - cat_length)
+            title += '*' * (30 - int((30 - cat_length) / 2) - cat_length)
+
+        title += '\n'
+
+        return title
+
+    def list_items(self):
+        items = ''
+
+        for item in self.ledger:
+            # first 23 char
+            if len(item['description']) > 24:
+                description = item['description'][:22]
+            else:
+                description = item['description']
+
+            # 2 decimal places and no more tha 7 char
+            amount = "{:.2f}".format(item['amount'])
+
+            if len(amount) > 8:
+                amount = amount[:6]
+
+            items += description + ' ' * (30 - len(description) - len(amount)) + amount + '\n'
+
+        return items
+
+    def total(self):
+        total = 'Total:' + ' ' + str(self.get_balance()) + '\n'
+
+    def __str__(self):
+        return f"{self.title()}{self.list_items()}{self.total()}"
