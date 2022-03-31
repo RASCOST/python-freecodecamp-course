2752c00f0beb71cd50440c1cef8d88a042f5d93e
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-03-31 22:09:44 +0200

    done

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -48,21 +48,3 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"Square(side={self.width})"
-
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
-
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
-
-rect.set_height(8)
-rect.set_width(16)
-print(rect.get_amount_inside(sq))
