4fdb92e442d7eca6cdcffbd651c0063adab47814
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-04-01 23:41:57 +0200

    created method draw

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -5,3 +5,11 @@ class Hat:
         for key, value in kwargs:
             for balls in range(value):
                 self.contents.append(key)
+
+    def draw(self, number_of_balls):
+        if number_of_balls > len(self.contents):
+            return self.contents
+
+        for number in range(number_of_balls):
+            index = random.randint(0, len(self.contents))
+            del self.content[index]
