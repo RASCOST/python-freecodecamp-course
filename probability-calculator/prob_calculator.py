e8659812fa72e646eabefddff5dc46d8d8f691a8
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-04-02 00:47:58 +0200

    fixed method draw

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -10,12 +10,17 @@ class Hat:
                 self.contents.append(key)
 
     def draw(self, number_of_balls):
+        balls = []
+
         if number_of_balls > len(self.contents):
             return self.contents
 
         for number in range(number_of_balls):
             index = random.randint(0, len(self.contents) - 1)
+            balls.append(self.contents[index])
             del self.contents[index]
+
+        return balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     probability = 0
